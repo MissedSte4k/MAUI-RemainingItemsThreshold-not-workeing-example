069f49638636cde1b3b93d851dcd1a7de5966828
MissedSte4k <36638029+MissedSte4k@users.noreply.github.com>
2022-09-06 11:26:01 +0300

    Add files via upload

---
+++ TestImages/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TestImages;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
